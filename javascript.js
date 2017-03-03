f8a50ecb3fc6a23bfb007b7f95082e6774eaf720
EJefferyCode <emmyjeffery99@gmail.com>
2017-03-02 22:27:24 -0500

    First commit R

---
+++ javascript.js
@@ -0,0 +1,37 @@
+function confirm($this) {
+
+    if ($this.value === "ss") {
+      alert("Sorry, but you are not eligible for this offer.");
+    }
+    
+    if($this.value === "op") {
+       alert("Sorry, but you are not eligible for this offer.");
+    }
+    
+    if ($this.value === "ua") {
+      alert("Sorry, but you are not eligible for this offer.");
+    }
+    
+    if ($this.value === "fo") {
+      alert("Sorry, but you are not eligible for this offer.");
+    }
+    
+    if ($this.value === "ov30") {
+      prompt("What is your exact annual income?");
+    }
+    if ($this.value === "ov45") {
+      alert("Sorry, but you are not eligible for this offer.");
+    }
+    
+    if ($this.value === "re") {
+     prompt("What are your monthly rent expenses?");
+    }
+    if ($this.value === "ow") {
+     alert("Sorry, but you are not eligible for this offer.");
+    }
+    
+    if ($this.value === "or") {
+     prompt("Please explain your living arrangement");
+    }
+}
+
